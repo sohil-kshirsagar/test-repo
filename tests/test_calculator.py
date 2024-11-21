6ec20416aebb2f0bf2bd8db31e5faf0667544efb
Sohil Kshirsagar <sohil@Sohils-MacBook-Pro.local>
2024-11-20 17:27:27 -0800

    Add divide

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,5 +1,8 @@
 import pytest
-from utils.calculator import add
+from utils.calculator import add, divide
 
 def test_add():
     assert add(1, 2) == 3
+
+def test_divide():
+    assert divide(10, 2) == 5
