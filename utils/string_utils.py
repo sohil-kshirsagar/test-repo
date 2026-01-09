28f3861db0df58f8617a8a1bda2ae4d69bc103ab
Sohil Kshirsagar <sohil@usetusk.ai>
2026-01-09 15:59:18 -0800

    minor fix

--- utils/string_utils.py
+++ utils/string_utils.py
@@ -4,5 +4,6 @@ def capitalize_first_letter(string):
     If string is None, return empty string
     Otherwise, return the string with the first letter capitalized
     """
+    if string is None or string == "":
+        return ""
     return string[0].upper() + string[1:]
-
