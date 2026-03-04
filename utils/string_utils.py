2c2942200a48de44bb45127c7ee1e55a74d021f6
Sohil Kshirsagar <sohil@usetusk.ai>
2026-03-04 13:14:19 -0800

    Revert "add utils (#170)"
    This reverts commit 399cc245bdee5dd278dfd4146bd34600adf0c45b.

--- utils/string_utils.py
+++
@@ -1,8 +0,0 @@
-def capitalize_first_letter(string):
-    """
-    Capitalize the first letter of a string
-    If string is None, return empty string
-    Otherwise, return the string with the first letter capitalized
-    """
-    return string[0].upper() + string[1:]
-
